e65595f863d26e8c78407b1cf01cf865be49ae19
Viru <viraschia@gmail.com>
2025-01-12 11:55:00 +0100

    start effects practice

--- 09-side-effects-place-picker/src/App.jsx
+++ 09-side-effects-place-picker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { sortPlacesByDistance } from './loc.js';
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -53,8 +53,7 @@ function App() {
       );
     }
   }
-
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces(prevPickedPlaces =>
       prevPickedPlaces.filter(place => place.id !== selectedPlace.current)
     );
@@ -65,7 +64,7 @@ function App() {
         storedIds.filter(locationId => locationId !== selectedPlace.current)
       )
     );
-  }
+  }, []);
 
   return (
     <>
--- 09-side-effects-place-picker/src/components/Modal.jsx
+++ 09-side-effects-place-picker/src/components/Modal.jsx
@@ -13,7 +13,7 @@ function Modal({ open, children, onClose }) {
 
   return createPortal(
     <dialog className="modal" ref={dialog} onClose={onClose}>
-      {children}
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
